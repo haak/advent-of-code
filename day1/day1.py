c15cfb67a587ac75df1befe8558f28288974e9c6
Alex Kelleher <alexkelleher@icloud.com>
2019-12-27 23:01:50 +0000

    day 1 finished

--- day1/day1.py
+++ day1/day1.py
@@ -16,28 +16,35 @@ def day1():
 
 
 
-mass = 12
 def get_fuel(mass):
     fuel = (math.floor(mass/3))-2
     return fuel
 
 
-day1()
+# day1()
 
 
 def get_fuel_recurring(mass):
-    while mass_remaining != 0:
+    mass_remaining = 0
+    total_fuel = 0
+    while mass_remaining > 0:
         fuel = (math.floor(mass/3))-2
         
 
 
-
-#mass =  14
-# fuel = 2
-#fuel = 
-
-
-def fuel_for_fuel(mass_of_fuel):
-    # use recursion for this. return once the fuel required is 0. if its not 0 
-    # call function with the mass.
-    return math.floor(mass_of_fuel/3)-2  
+def day1p2():
+    file = open("in.txt", "r")
+    # out = open("out.txt","w")
+    total_fuel = 0
+    for line in file:
+        fuel = get_fuel(int(line))
+        total_fuel += fuel
+        # print(fuel)
+        while fuel > 0:
+            fuel = get_fuel(int(fuel))
+            if fuel > 0:
+                total_fuel += fuel
+            # print(fuel)
+    print("total fuel: " + str(total_fuel))
+        
+day1p2()
