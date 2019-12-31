dd5e4b2d3cf253544e37da4f6ee9c2f184a7bab9
Alex Kelleher <alexkelleher@icloud.com>
2019-12-31 23:41:46 +0000

    started day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,10 +1,24 @@
 def main():
-    file = open("in.txt","a")
-    wireA = file.readline()
-    wireB = file.readline()
-
-    direction = wire[0]
+    f1 = open("in.txt","r")
+    wireA = f1.readline()
+    wireB = f1.readline()
+    # print(wireA)
+    x,y = 0
+    locs = {}
+    # direction = wire[0]
+    for step in wireA:
+        direction = step[0]
+        delta = step[1:]
+        if direction == "U":
+            return
+        elif direction == "D":
+            return
+        elif direction == "L":
+            return
+        elif direction == "R":
+            return
     
+
 
 
     return
