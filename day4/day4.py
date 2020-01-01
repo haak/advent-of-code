08d6fe5549afbd872f94ef926d54ecf89573fe1b
Alex Kelleher <alexkelleher@icloud.com>
2020-01-01 12:00:00 +0000

    update gitignore

--- day4/day4.py
+++
@@ -1,6 +0,0 @@
-ans = 0
-for s in range(245182, 790572 + 1):
-    x = str(s)
-    if any(d*2 in x and d*3 not in x for d in '0123456789') and all(l<=r for l,r in zip(x[::], x[1::])):
-        ans += 1
-print(ans)
