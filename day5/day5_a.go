2afd25eb9fd2c0b0a6a361f1c7c05dcd8d0a57be
Alex Kelleher <alexkelleher@icloud.com>
2020-08-24 10:26:38 +0100

    day5

--- day5/day5_a.go
+++ day5/day5_a.go
@@ -11,14 +11,10 @@ import (
 
 func main() {
 
-	
-
-
 	// Putting temp input here for TESTING
 
-	line := "1002,4,3,4,33"
-
-
+	// line := "1,225,6,6"
+	line := readInput()
 	// fmt.Println("line = ", line)
 	// fmt.Println("\n")
 
@@ -30,7 +26,8 @@ func main() {
 	// fmt.Println(slice)
 
 	var num []int
-	var programCounter int 
+
+	var programCounter int
 	// fmt.Println("num", num)
 	// fmt.Println("\n")
 
@@ -42,6 +39,7 @@ func main() {
 		num = append(num, n)
 	}
 
+	num = append(num, 0, 0)
 	// OPCODES:
 	// 99 	STOP
 	// 1	ADD
@@ -52,15 +50,81 @@ func main() {
 
 	// loop through numbers
 	for programCounter > -1 {
-		// takes the input and parses it then calls runInstruction based on the input 
+		// takes the input and parses it then calls runInstruction based on the input
 
-		opcode := num[programCounter]
+		instruction := num[programCounter]
 		// this could be up to 4 numbers
-		fmt.Println(opcode)
 
+		opCode := instruction % 100
+		fmt.Println(instruction)
+		fmt.Println(opCode)
 
+		parameters := fmt.Sprintf("%.2d", num[programCounter]/100)
+		// fmt.Println(parameters)
 
-		// parse 
+		if len(parameters) == 2 {
+			paramMode3 := 0
+			if paramMode3 == 0 {
+
+			}
+
+			// set modeOf3rdParam to be position mode
+		}
+
+		paramMode1 := parameters[1:]
+		// fmt.Println("paramMode1", paramMode1)
+
+		paramMode2 := parameters[:1]
+		// fmt.Println("paramMode2", paramMode2)
+
+		a := num[programCounter+1]
+		if paramMode1 == "0" && (opCode == 1 || opCode == 2) {
+			a = num[a]
+		}
+		b := num[programCounter+2]
+		if paramMode2 == "0" && (opCode == 1 || opCode == 2) {
+			b = num[b]
+		}
+		fmt.Println("programCounter: ", programCounter)
+		switch opCode {
+
+		case 1:
+			
+			fmt.Println("case 1:", a," ", b, " mode1: ", paramMode1, " mode2: ", paramMode2, " location ", num[num[programCounter+3]])
+			fmt.Println(num)
+			num[num[programCounter+3]] = a + b // num[dest] = num 1 + num 2
+			programCounter += 4
+		case 2:
+			fmt.Println("case 2")
+			num[num[programCounter+3]] = a * b
+			programCounter += 4
+
+		case 3:
+			fmt.Println("case 3")
+
+			num[num[programCounter+1]] = num[programCounter+1]
+			fmt.Println(num[programCounter+1])
+			fmt.Println(num[num[programCounter+1]])
+			programCounter += 2
+		case 4:
+			fmt.Println("case 4")
+			fmt.Println(num[programCounter+1])
+			programCounter += 2
+
+		case 99:
+			programCounter = -1
+
+		default:
+			fmt.Println("Error at instruction number ", programCounter)
+			os.Exit(3)
+			
+
+		}
+		// break
+
+		// 1002 ==
+
+		// parse
 		// 1004 %100 = 4
 		// 4 % 100 = 4
 		// fmt.Println(num[i])
@@ -71,7 +135,6 @@ func main() {
 		// // check opcode
 		// if num[i] == 1 {
 		// 	// run func 1
-
 
 		// 	num[num[i+3]] = a + b // num[dest] = num 1 + num 2
 		// } else if num[i] == 2 {
@@ -87,9 +150,8 @@ func main() {
 		// } else if num[i] == 4 {
 		// 	fmt.Println( num[i+1])
 		// 	return
-		// 	// should move the number 
+		// 	// should move the number
 		// 	// opcode 4
-
 
 		// } else if num[i] == 99 {
 
@@ -99,7 +161,6 @@ func main() {
 		// 	fmt.Println(i)
 		// 	break
 		// }
-
 
 	}
 	fmt.Println(num[0])
@@ -111,17 +172,15 @@ func main() {
 
 }
 
-
-
-func runInstruction(){
+func runInstruction() {
 	// position in memory == address
 	// value add
-	//OPCODES: indicates what to do 
-	// 1: 3 inputs 
+	//OPCODES: indicates what to do
+	// 1: 3 inputs
 	// 2: 3 inputs
-	//  99: no inputs 
+	//  99: no inputs
 	// 3, 4
-	//VERBS 
+	//VERBS
 	//NOUNS
 	//PARAMETER MODES:
 	// 0,1
@@ -129,9 +188,7 @@ func runInstruction(){
 	// mode 1: 50 means 50
 }
 
-
-
-func readInput() (line string){
+func readInput() (line string) {
 
 	file, err := os.Open("in.txt")
 	if err != nil {
@@ -155,7 +212,6 @@ func readInput() (line string){
 		}
 	}
 	line = scanner.Text()
-	return line 
-
+	return line
 
 }
